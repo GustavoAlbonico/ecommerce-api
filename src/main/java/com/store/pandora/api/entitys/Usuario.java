c89441944f41cad5196acbdba41105cfb84b8fb8
Usuario <gustavoalbonicogoncalves@hotmail.com>
2024-05-18 21:08:30 -0300

    Adicionando estrutura base e fazendo post,get all,get one,put de usuario

---
+++ src/main/java/com/store/pandora/api/entitys/Usuario.java
@@ -0,0 +1,59 @@
+package com.store.pandora.api.entitys;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.SQLDelete;
+
+import java.time.LocalDateTime;
+
+@Entity
+@SQLDelete(sql = "UPDATE usuario SET deleted_at = now() WHERE id=?")
+@SQLRestriction("deleted_at is null")
+public class Usuario {
+
+    @Id
+    @Column(name = "usuario_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
---
+++ src/main/java/com/store/pandora/api/useCases/usuario/domains/UsuarioResponseDom.java
@@ -0,0 +1,46 @@
+package com.store.pandora.api.useCases.usuario.domains;
+
+import java.time.LocalDateTime;
+
+public class UsuarioResponseDom {
+
+    private Long id;
+
+    private String login;
+
+    private String senha;
+
+    private LocalDateTime deleted_at;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(LocalDateTime deleted_at) {
+        this.deleted_at = deleted_at;
+    }
+}
