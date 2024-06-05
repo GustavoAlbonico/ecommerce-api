8f9a9a610b20736d420ea224f1a5bb80454209a0
juliatibes <julia.tibes@gmail.com>
2024-06-05 20:58:54 -0300

    feature/EC-API-07-cliente - Relacionamento da lista de endereços com a classe de Cliente

---
+++ src/main/java/com/store/pandora/api/useCases/cliente/domains/ClienteEnderecoResponseDom.java
@@ -0,0 +1,6 @@
+package com.store.pandora.api.useCases.cliente.domains;
+
+import com.store.pandora.api.useCases.endereco.domains.EnderecoResponseDom;
+
+public class ClienteEnderecoResponseDom extends EnderecoResponseDom {
+}
